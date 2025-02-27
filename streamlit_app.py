b87cf2f2150139b128b83917c3e3ba7fddecf1d9
dewi-pujoningsih <dewi@apps.aka.ac.id>
2025-02-27 14:15:45 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,8 +2,7 @@ import streamlit as st
 st.title(":blue[Kalkulator Aritmatika]")
 number1 = st.number_input("masukkan angka 1")
 number2 = st.number_input("masukkan angka 2")
-st.button("Hasil kali", type="primary")
-if st.button("Hasil kali"):
+if st.button("Hasil kali", type="primary"):
     st.write(number1*number2)
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
