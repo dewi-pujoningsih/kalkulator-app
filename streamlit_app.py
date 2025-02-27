ca02e3d539ebf5c8907fbeba9c59669fc4329498
dewi-pujoningsih <dewi@apps.aka.ac.id>
2025-02-27 14:03:56 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,5 @@
 import streamlit as st
-st.title("Kalkulator Aritmatika")
+st.title(":blue[Kalkulator Aritmatika]")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
