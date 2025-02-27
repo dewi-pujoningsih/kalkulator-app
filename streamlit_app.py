0d705accf54c647754eed2cd24f4c390f6542f72
dewi-pujoningsih <dewi@apps.aka.ac.id>
2025-02-27 13:56:53 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,10 @@
 import streamlit as st
-
+pip install st-annotated-text
+from annotated_text import annotated_text
+annotated_text(
+    "This ",
+    ("is", "verb")
+)
 st.title("🎈 My new app")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
