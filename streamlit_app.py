40cde1d209b01338e0a0d2fc274248cd40584ebe
dewi-pujoningsih <dewi@apps.aka.ac.id>
2025-02-27 13:58:08 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,11 +1,5 @@
 import streamlit as st
-pip install st-annotated-text
-from annotated_text import annotated_text
-annotated_text(
-    "This ",
-    ("is", "verb")
-)
-st.title("🎈 My new app")
+st.title("Kalkulator Aritmatika")
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
