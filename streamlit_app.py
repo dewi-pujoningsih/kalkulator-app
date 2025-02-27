6cb2bcc328737aa8c642e480bcef1e54a98b2e1d
dewi-pujoningsih <dewi@apps.aka.ac.id>
2025-02-27 14:27:11 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ number1 = st.number_input("masukkan angka 1")
 number2 = st.number_input("masukkan angka 2")
 if st.checkbox("hasil kali"):
     st.write(number1*number2)
-elif st.checkbox("hasil bagi"):
+if st.checkbox("hasil bagi"):
     st.write(number1/number2)
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
