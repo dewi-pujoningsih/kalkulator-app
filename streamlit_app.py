c18879c8fe60bf77565c3b7dd987ebf3ba40614b
dewi-pujoningsih <dewi@apps.aka.ac.id>
2025-02-27 15:44:47 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,13 +4,3 @@ def page_2():
 pg = st.navigation([st.Page("page_1.py"), st.Page(page_2)])
 pg.run()
 
-st.title(":blue[Kalkulator Aritmatika]:rocket:")
-number1 = st.number_input("masukkan angka 1")
-number2 = st.number_input("masukkan angka 2")
-if st.checkbox("hasil kali"):
-    st.write(number1*number2)
-if st.checkbox("hasil bagi"):
-    st.write(number1/number2)
-st.write(
-    "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
-)
