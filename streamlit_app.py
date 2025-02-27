605fc32c9f1bba4247708be3ebda836d9daa9893
dewi-pujoningsih <dewi@apps.aka.ac.id>
2025-02-27 15:43:51 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,4 +1,9 @@
 import streamlit as st
+def page_2():
+    st.title("Page 2")
+pg = st.navigation([st.Page("page_1.py"), st.Page(page_2)])
+pg.run()
+
 st.title(":blue[Kalkulator Aritmatika]:rocket:")
 number1 = st.number_input("masukkan angka 1")
 number2 = st.number_input("masukkan angka 2")
