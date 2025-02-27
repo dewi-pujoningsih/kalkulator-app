915a78cc4b0829d00640fc1a309e86f00156d3d6
dewi-pujoningsih <dewi@apps.aka.ac.id>
2025-02-27 14:07:05 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,5 +1,8 @@
 import streamlit as st
 st.title(":blue[Kalkulator Aritmatika]")
+number1 = st.number_input("masukkan angka 1")
+number2 = st.number_input("masukkan angka 2")
+
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
