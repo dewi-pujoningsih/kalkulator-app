e41e1ca49179d540c011026cc0eae92763ed5be0
dewi-pujoningsih <dewi@apps.aka.ac.id>
2025-02-27 14:21:14 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,6 +4,8 @@ number1 = st.number_input("masukkan angka 1")
 number2 = st.number_input("masukkan angka 2")
 if st.button("Hasil kali", type="primary"):
     st.write(number1*number2)
+elif if st.button("Hasil bagi", type="primary"):
+    st.write(number1/number2)
 st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
