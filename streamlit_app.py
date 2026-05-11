235211a04a73bf60a62e550ba537f3845f6a6f8e
dewi-pujoningsih <dewi@apps.aka.ac.id>
2026-05-11 16:58:55 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -16,13 +16,4 @@ elif tiga.button("operasi kuadrat") :
     st.write(number**2)
 if st.button("reset"):
     st.rerun()
-    #hasil=math.sqrt(number1)
-    #st.success(f"Faktorial dari {number1} adalah: **{hasil}**")
-#number2 = st.number_input("masukkan angka 2")
-#if st.checkbox("hasil kali"):
-    #st.write(number1*number2)
-#if st.checkbox("hasil bagi"):
-    #st.write(number1/number2)
-#st.write(
-  #  "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/).")
-
+   
