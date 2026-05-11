912ce40ea0853bf469aa59378769b86278ccc449
dewi-pujoningsih <dewi@apps.aka.ac.id>
2026-05-11 17:06:28 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,23 +3,17 @@ import math
 st.title(":blue[Kalkulator Aritmatika]:rocket:")
 st.header(":red[Aplikasi untuk menghitung operasi aritmatika]")
 number = st.number_input("masukkan angka=",min_value=0)
-satu,dua,tiga=st.columns(3)
-empat,lima=st.columns(2)
-satu=st.button("operasi faktorial")
-dua=st.button("operasi akar kuadrat")
-tiga=st.button("operasi kuadrat")
-empat=st.button("operasi logaritma")
-lima=st.button("operasi ln")
-if satu:
+satu,dua,tiga,empat,lima=st.columns(5)
+if satu.button("operasi faktorial"):
     number = int(number)
     st.write(math.factorial(number))
-elif dua:
+elif dua.button("operasi akar kuadrat"):
     st.write(math.sqrt(number))
-elif tiga :
+elif tiga.button("operasi kuadrat") :
     st.write(number**2)
-elif empat :
+elif empat.button("operasi logaritma") :
     st.write(math.log(number))
-elif lima:
+elif lima.button("operasi ln"):
     st.write(math.ln(number))
 if st.button("reset"):
     st.rerun()
