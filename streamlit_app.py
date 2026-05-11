a7bd8cba831dbb846e751f37df0ee93453609453
dewi-pujoningsih <dewi@apps.aka.ac.id>
2026-05-11 16:21:40 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,7 +4,7 @@ st.title(":blue[Kalkulator Aritmatika]:rocket:")
 st.header(":red[Aplikasi untuk menghitung operasi aritmatika]")
 if st.button("hasil faktorial"):
     number1 = int(st.number_input("masukkan angka 1"))
-    st.write(math.factorial(number1))
+    st.success(math.factorial(number1))
     #hasil=math.sqrt(number1)
     #st.success(f"Faktorial dari {number1} adalah: **{hasil}**")
 #number2 = st.number_input("masukkan angka 2")
