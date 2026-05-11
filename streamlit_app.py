fbbb897f256238941581aef6acb0009e4ff8cb3a
dewi-pujoningsih <dewi@apps.aka.ac.id>
2026-05-11 14:12:03 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,7 +5,7 @@ st.header(":red[Aplikasi untuk menghitung nilai faktorial]")
 number1 = st.number_input("masukkan angka 1")
 if st.button("hasil faktorial"):
     hasil=math.factorial(number1)
-    st.success(f"{number1} !={hasil}")
+    st.success(hasil)
 #number2 = st.number_input("masukkan angka 2")
 #if st.checkbox("hasil kali"):
     #st.write(number1*number2)
