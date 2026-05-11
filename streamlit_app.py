b4199ef4a46114b41d418e346b3459ef40bbe997
dewi-pujoningsih <dewi@apps.aka.ac.id>
2026-05-11 13:47:01 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -3,6 +3,8 @@ import math
 st.title(":blue[Kalkulator Aritmatika]:rocket:")
 st.header(":red[Aplikasi untuk menghitung nilai faktorial]")
 number1 = st.number_input("masukkan angka 1")
+if st.checkbox("hasil faktorial"):
+    st.write(math.factorial(number1))
 number2 = st.number_input("masukkan angka 2")
 if st.checkbox("hasil kali"):
     st.write(number1*number2)
