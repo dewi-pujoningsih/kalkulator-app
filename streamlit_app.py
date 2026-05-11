5235651b80c9b89a96b391776efdd40a66661fa1
dewi-pujoningsih <dewi@apps.aka.ac.id>
2026-05-11 13:52:03 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -6,12 +6,12 @@ number1 = st.number_input("masukkan angka 1")
 if st.button("hasil faktorial"):
     hasil=math.factorial(number1)
     st.success(f"{number1} !={hasil}")
-number2 = st.number_input("masukkan angka 2")
-if st.checkbox("hasil kali"):
-    st.write(number1*number2)
-if st.checkbox("hasil bagi"):
-    st.write(number1/number2)
-st.write(
+#number2 = st.number_input("masukkan angka 2")
+#if st.checkbox("hasil kali"):
+    #st.write(number1*number2)
+#if st.checkbox("hasil bagi"):
+    #st.write(number1/number2)
+#st.write(
     "Let's start building! For help and inspiration, head over to [docs.streamlit.io](https://docs.streamlit.io/)."
 )
 
