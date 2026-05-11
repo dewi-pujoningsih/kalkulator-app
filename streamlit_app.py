3dcc5aea7e8cc4b06218914e7e5f4992ce9a5abb
dewi-pujoningsih <dewi@apps.aka.ac.id>
2026-05-11 16:39:54 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,11 +2,19 @@ import streamlit as st
 import math 
 st.title(":blue[Kalkulator Aritmatika]:rocket:")
 st.header(":red[Aplikasi untuk menghitung operasi aritmatika]")
-st.button("hasil faktorial"):
-    number1 = int(st.number_input("masukkan angka 1",min_value=0, format="%0.1f"))
-    if st.button("hitung"):
-        hasil=math.factorial(number1)
-        st.success(f"hasil {number1}!={hasil}")
+number = st.number_input("masukkan angka 1",min_value=0, format="%0.1f")
+satu,dua,tiga=st.column(3)
+satu=st.button("operasi faktorial")
+dua=st.button("operasi akar kuadrat")
+tiga=st.button("operasi kuadrat")
+if satu:
+    number = int(number)
+    st.write(math.factorial(number))
+elif dua:
+    st.write(math.sqrt(number))
+elif tiga :
+    st.write(math.sqr(number)
+           
     #hasil=math.sqrt(number1)
     #st.success(f"Faktorial dari {number1} adalah: **{hasil}**")
 #number2 = st.number_input("masukkan angka 2")
